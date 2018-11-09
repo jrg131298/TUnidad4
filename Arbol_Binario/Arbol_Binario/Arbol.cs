09155cdee2b908b0d0adf15b3c6d66b7b343c76b
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2018-11-09 11:13:22 -0800

    Arbol

---
+++ Arbol_Binario/Arbol_Binario/Arbol.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    public class Arbol //Clase arbol
+    {
+        public int altura; //Variable que guardara el valor de la altura del arbol
+        class Nodo //Clase nodo
+        {
+            public int info; //Guarda el valor que toma el nodo
+            public Nodo izq, der; //Dos punteros
+        }
+        Nodo raiz; //Se crea un objeto raiz;
+
+        class Dato //Clase dato
+        {
+            public string lugar; //Almacena el camino al dato ingresado
+            public int posicionaltura; //Almacena el nivel de cada dato
+        }
+        List<Dato> weita = new List<Dato>(); //Lista que guarda objetos de la clase Dato
+
+        public Arbol() //Constructor de la clase Arbol
+        {
+            raiz = null;
+        }
+
+        public void Insertar(int info) //Metodo el cual inserta los datos en el arbol
+        {
+            string Concatenador = ""; //Nos permite guardar la ruta de cada nodo
+            int contador = 1; //Contador
+            Dato dato = new Dato(); //Se crea un nuevo nodo el cual va a guardar el dato ingresado
+            Nodo nuevo;
+            nuevo = new Nodo();
+            nuevo.info = info;
+            nuevo.izq = null;
+            nuevo.der = null;
+            if (raiz == null) //Significa que es el primer dato y lo almacena directamente
+            {
+                raiz = nuevo;
+                Concatenador = Convert.ToString(raiz.info);
+            }
+            else //Significa que ya existe almenos un dato en el arbol
+            {
+                Nodo anterior = null, recorrido;
+                recorrido = raiz;
+                while (recorrido != null) //Nos permite localizar el ultimo nodo donde se va almacenar el dato ingresado
+                {
+                    anterior = recorrido;
+                    if (info < recorrido.info)
+                    {
+                        Concatenador = Convert.ToString(recorrido.info) + "<-"; ;//Guarda la direccion
+                        recorrido = recorrido.izq;
+                    }
+                    else
+                    {
+                        Concatenador = Convert.ToString(recorrido.info) + "<-";//Guarda la direccion
+                        recorrido = recorrido.der;
+                    }
+                    contador++;
+                }
+                //Se ingresa el dato dependiendo si es menor o mayor al ultimo dato
+                if (info < anterior.info)
+                    anterior.izq = nuevo;
+                else
+                    anterior.der = nuevo;
+                dato.posicionaltura = contador; //Se guarda el nivel que se encuentra el dato
+                Concatenador = Concatenador + Convert.ToString(info); //Se guarda la direccion
+                dato.lugar = Concatenador; //Se guarda la direccion
+                weita.Add(dato); //Se guarda el objeto dato, en la lista weita
+            }
+        }
+
+
+        private void ImprimirPre(Nodo recorrido) //Metodo recursivo que imprime los datos en PreOrden
+        {
+            if (recorrido != null)
+            {
+                Console.Write(recorrido.info + " ");
+                ImprimirPre(recorrido.izq);
+                ImprimirPre(recorrido.der);
+            }
+        }
+
+        public void ImprimirPre() //Metodo recursivo que imprime los datos en PreOrden
+        {
+            ImprimirPre(raiz);
+            Console.WriteLine();
+        }
+
+        private void ImprimirEntre(Nodo recorrido) //Metodo recursivo que imprime los datos en InOrden
+        {
+            if (recorrido != null)
+            {
+                ImprimirEntre(recorrido.izq);
+                Console.Write(recorrido.info + " ");
+                ImprimirEntre(recorrido.der);
+            }
+        }
+
+        public void ImprimirEntre()//Metodo recursivo que imprime los datos en InOrden
+        {
+            ImprimirEntre(raiz);
+            Console.WriteLine();
+        }
+
+
+        private void ImprimirPost(Nodo recorrido) //Metodo recursivo que imprime los datos en PostOrden
+        {
+            if (recorrido != null)
+            {
+                ImprimirPost(recorrido.izq);
+                ImprimirPost(recorrido.der);
+                Console.Write(recorrido.info + " ");
+            }
+        }
+
+        public void ImprimirPost() //Metodo recursivo que imprime los datos en PostOrden
+        {
+            ImprimirPost(raiz);
+            Console.WriteLine();
+        }
+
+        public void ImpresionDeLasWeas() //Metodo recursivo que imprime los datos restantes
+        {
+            int Cont = 1;
+            foreach(Dato Item in weita) //Permite encontrar la altura del arbol
+            {
+                if(Cont == 1)
+                {
+                    altura = Item.posicionaltura;
+                }
+                else
+                {
+                    if (Item.posicionaltura > altura)
+                    {
+                        altura = Item.posicionaltura;
+                    }
+                }
+                Cont++;
+            }
+            //Impresion de los datos
+            Console.WriteLine("\nLa raiz es: {0}", raiz.info);
+            Console.WriteLine("Nodo izquierdo: {0}", raiz.izq.info);
+            Console.WriteLine("Nodo derecho: {0}", raiz.der.info);
+            Console.WriteLine("La altura es: {0}", altura);
+            Console.WriteLine("La cantidad de niveles es: {0}", (altura - 1));
+            Console.WriteLine("Camino(s) al(los) ultimo(s) elemento(s):");
+            Cont = 1;
+            foreach (Dato item in weita) //Busca la(s) direccion(es) de el(los) ultimo(s) dato(s) y la(los) imprime
+            {
+                if (item.posicionaltura == altura)
+                {
+                    Console.Write(raiz.info + "<-" + item.lugar + "\n");
+                }
+            }
+        }
+    }
+}
---
+++ Arbol_Binario/Arbol_Binario/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arbol abo = new Arbol(); //Se crea un objeto de la clase arbol
+            //Se insertan datos en nuestro arbol
+            abo.Insertar(100);
+            abo.Insertar(50);
+            abo.Insertar(25);
+            abo.Insertar(75);
+            abo.Insertar(150);
+            //Impresion en preorden
+            Console.WriteLine("Impresion preorden: ");
+            abo.ImprimirPre();
+            //Impresion en entreorden
+            Console.WriteLine("Impresion inorden: ");
+            abo.ImprimirEntre();
+            //Impresion en postorden
+            Console.WriteLine("Impresion postorden: ");
+            abo.ImprimirPost();
+            //Impresion de los datos
+            abo.ImpresionDeLasWeas();
+            Console.WriteLine("\nPreciona una tecla para salir del programa.");
+            Console.ReadKey();
+        }
+    }
+
+}
